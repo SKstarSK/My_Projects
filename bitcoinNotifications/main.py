330fb77f65ec7e7ed8832dea74bed60b4a42642a
SKstarSK <bolotin.n95@gmail.com>
2020-07-16 18:12:21 +0300

    Подключил функцию автоизменения масштаба autoscale

--- bitcoinNotifications/main.py
+++ bitcoinNotifications/main.py
@@ -1,29 +1,32 @@
 import requests
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import datetime
 import time
 
 xArray = []
 yArray = []
 bitcoinApiUrl = 'https://blockchain.info/ru/ticker'
-fig = plt.figure()
-ax = fig.add_subplot(111)
-# plt.ion()
-
+fig, ax = plt.subplots()
+[line] = ax.step(5,0)
 fig.show()
 fig.canvas.draw()
+t = 0
 
 while True:
-
     response = requests.get(bitcoinApiUrl)
     responseJson = response.json()
 
-    xArray.append(datetime.datetime.today().strftime("%H.%M.%S"))
+    # xArray.append(datetime.datetime.today().strftime("%H.%M.%S"))
+    xArray.append(t)
     yArray.append(responseJson['RUB']['buy'])
+    line.set_xdata(xArray)
+    line.set_ydata(yArray)
 
+    ax.relim()
+    ax.autoscale_view(True, True, True)
     ax.clear()
     ax.plot(xArray, yArray)
     time.sleep(5)
+    t += 5
     fig.canvas.draw()
-    print(xArray)
-    print(yArray)
