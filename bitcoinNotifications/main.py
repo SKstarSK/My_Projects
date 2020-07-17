a6833ed3db2b9656d72822f9d0146320e6c79c78
SKstarSK <bolotin.n95@gmail.com>
2020-07-17 16:02:21 +0300

    Вернул файл к старому формату. Необходима доработка момента подстановки даты на ось х.

--- bitcoinNotifications/main.py
+++ bitcoinNotifications/main.py
@@ -7,26 +7,22 @@ import time
 xArray = []
 yArray = []
 bitcoinApiUrl = 'https://blockchain.info/ru/ticker'
-fig, ax = plt.subplots()
-[line] = ax.step(5,0)
+fig = plt.figure()
+ax = plt.subplot(111)
+# plt.ion()
+
 fig.show()
 fig.canvas.draw()
-t = 0
 
 while True:
+
     response = requests.get(bitcoinApiUrl)
     responseJson = response.json()
 
-    # xArray.append(datetime.datetime.today().strftime("%H.%M.%S"))
-    xArray.append(t)
+    xArray.append(datetime.datetime.today().strftime("%H.%M.%S"))
     yArray.append(responseJson['RUB']['buy'])
-    line.set_xdata(xArray)
-    line.set_ydata(yArray)
 
-    ax.relim()
-    ax.autoscale_view(True, True, True)
     ax.clear()
     ax.plot(xArray, yArray)
     time.sleep(5)
-    t += 5
     fig.canvas.draw()
