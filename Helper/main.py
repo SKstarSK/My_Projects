41d1dd58c8292c17f1f098f78bbd4c95e4a49880
SKstarSK <bolotin.n95@gmail.com>
2020-07-17 16:01:15 +0300

    Исправил ошибку при удалении директории со вложенными файлами. Подключил библиотеку shutil

--- Helper/main.py
+++ Helper/main.py
@@ -1,10 +1,14 @@
 from watchdog.observers import Observer
 import os
 import time
+import shutil
 from watchdog.events import FileSystemEventHandler
 
+
 class Handler(FileSystemEventHandler):
+
     def on_modified(self, event):
+
         findIt = False
         for filename in os.listdir(folder_track):
             extension = filename.split(".")
@@ -28,21 +32,27 @@ class Handler(FileSystemEventHandler):
                     handle.replace_file(folder_track, filename, myWays[4])
                 else:
                     os.remove(folder_track + "/" + filename)
-            elif len(extension) > 1 and ("doc" in extension or "docx" in extension or "pdf" in extension or "ppt" in extension or "pptx" in extension or "xlsx" in extension):
+            elif len(extension) > 1 and (
+                    "doc" in extension or "docx" in extension or "pdf" in extension or "ppt" in extension or "pptx" in extension or "xlsx" in extension):
                 if filename not in os.listdir(folder_track + "/" + myWays[5]):
                     handle.replace_file(folder_track, filename, myWays[5])
                 else:
                     os.remove(folder_track + "/" + filename)
-            elif len(extension) > 1 and (os.path.isfile(folder_track+"/"+filename)) or (os.path.isdir(folder_track+"/"+filename)):
+            if len(extension) > 1 and (os.path.isfile(folder_track + "/" + filename)) or (
+            os.path.isdir(folder_track + "/" + filename)):
                 for dirname in myWays:
                     if dirname in extension:
                         findIt = True
 
                 if not findIt:
                     if filename not in os.listdir(folder_track + "/" + myWays[6]):
-                        handle.replace_file(folder_track, filename, myWays[6])
+                        # handle.replace_file(folder_track, filename, myWays[6])
+                        shutil.move(folder_track+"/"+filename,folder_track+"/"+myWays[6])
                     else:
-                        os.remove(folder_track + "/" + filename)
+                        if os.path.isfile(folder_track + "/" + filename):
+                            os.remove(folder_track + "/" + filename)
+                        else:
+                            shutil.rmtree(folder_track + "/" + filename)
                 findIt = False
 
     def replace_file(self, folder_track, filename, way):
@@ -50,7 +60,9 @@ class Handler(FileSystemEventHandler):
         new_path = folder_track + "/" + way + "/" + filename
         os.replace(file, new_path)
 
-folder_track = '/Users/bolot/Downloads'
+
+# folder_track = '/Users/bolot/Downloads' #For Windows
+folder_track = '/media/username/Acer/Users/bolot/Downloads' #For Linux
 myWays = ['Downloads', 'Photo', 'Archives', 'Video', 'Programms', 'Docs', 'Other']
 photoExtensions = ["jpg", "png", "svg"]
 findIt = False
@@ -61,7 +73,7 @@ observer.schedule(handle, folder_track, recursive=True)
 observer.start()
 
 try:
-    while(True):
+    while (True):
         time.sleep(10)
 except KeyboardInterrupt:
     observer.stop()
